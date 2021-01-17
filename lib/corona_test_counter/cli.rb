1906b287d878fb2cb3e5ce0387631ea8192921f7
Devin Wilson <craigvin84@yahoo.com>
2021-01-17 20:01:30 +0000

    add states

--- lib/corona_test_counter/cli.rb
+++ lib/corona_test_counter/cli.rb
@@ -1,5 +1,67 @@
-class CoronaTestCounter::CLI 
-  def call 
+class CoronaTestCounter::CLI
+  def call
     puts "Welcome to the Covid Test Counter"
+    get_available_states
+    get_user_state
+    #get_counts(state)
+    #list_counts
+  end
+
+    def get_available_states
+      @states = ["Alaska",
+                  "Alabama",
+                  "Arkansas",
+                  "Arizona",
+                  "California",
+                  "Colorado",
+                  "Connecticut",
+                  "Delaware",
+                  "Florida",
+                  "Georgia",
+                  "Hawaii",
+                  "Iowa",
+                  "Idaho",
+                  "Illinois",
+                  "Indiana",
+                  "Kansas",
+                  "Kentucky",
+                  "Louisiana",
+                  "Massachusetts",
+                  "Maryland",
+                  "Maine",
+                  "Michigan",
+                  "Minnesota",
+                  "Missouri",
+                  "Mississippi",
+                  "Montana",
+                  "North Carolina",
+                  " North Dakota",
+                  "Nebraska",
+                  "New Hampshire",
+                  "New Jersey",
+                  "New Mexico",
+                  "Nevada",
+                  "New York",
+                  "Ohio",
+                  "Oklahoma",
+                  "Oregon",
+                  "Pennsylvania",
+                  "Rhode Island",
+                  "South Carolina",
+                  "South Dakota",
+                  "Tennessee",
+                  "Texas",
+                  "Utah",
+                  "Virginia",
+                  "Vermont",
+                  "Washington",
+                  "Wisconsin",
+                  "West Virginia",
+                  "Wyoming"]
+
+  def get_user_state
+    #list states
+    @states.each_with_index {|index, state|
+    puts "#{index} #{state}"}
   end
 end
