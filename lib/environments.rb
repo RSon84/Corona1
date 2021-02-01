54dfec0fb9c23d213f91c2bb753c1d6f02e19f15
Devin Wilson <craigvin84@yahoo.com>
2021-02-01 19:08:14 +0000

    colorize

--- lib/environments.rb
+++ lib/environments.rb
@@ -5,6 +5,7 @@ require_relative "./corona_test_counter/scraper"
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
+require 'colorize'
 
 module CoronaTestCounter
   class Error < StandardError; end
