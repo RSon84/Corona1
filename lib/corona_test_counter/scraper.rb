6e70fa454d4f16c70f68691f1e4855293b307de9
Devin Wilson <craigvin84@yahoo.com>
2021-02-06 21:12:45 +0000

    fix #

--- lib/corona_test_counter/scraper.rb
+++ lib/corona_test_counter/scraper.rb
@@ -1,11 +1,13 @@
 class CoronaTestCounter::Scraper
 
 def self.scrape_states
-  CoronaTestCounter::CLI.get_user_state(self)
-  #selected_state = gets.downcase.strip
 
-  doc = Nokogiri::HTML(open("https://covidtracking.com/data/state/#{selected_state}/tests-viral"))
+
+
+  doc = Nokogiri::HTML(open("https://covidtracking.com/data/state/utah/tests-viral"))
   state_count1 =  doc.xpath("//tbody/tr[1]/td[6]/span[2]").text
+
+  puts  "#{state_count1}"
 
 
 end
