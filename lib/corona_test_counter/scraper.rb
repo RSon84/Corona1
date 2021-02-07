104fa54ff70dc418591fedc00ee6e2e1228393e8
Devin Wilson <craigvin84@yahoo.com>
2021-02-07 22:16:41 +0000

    alaska

--- lib/corona_test_counter/scraper.rb
+++ lib/corona_test_counter/scraper.rb
@@ -3,15 +3,15 @@ class CoronaTestCounter::Scraper
 def self.scrape_states
 
 
-if self == "alaska"
+#if self == "alaska"
   doc = Nokogiri::HTML(open("https://covidtracking.com/data/state/alaska/tests-viral"))
 
-  state_date =  doc.css("div.container.ddf6f:nth-child(2) div.d3a4d table.e1fd0._4545b:nth-child(13) tbody:nth-child(2) tr:nth-child td.b6f76._4c288:nth-child(1) > span.e0025").text
+  state_date =  doc.css("div.container.ddf6f:nth-child(2) div.d3a4d table.e1fd0._4545b:nth-child(13) tbody:nth-child(2) tr:nth-child(1) td.b6f76._4c288:nth-child(1) > span.e0025").text
 
-  state_tests = doc.css("div.container.ddf6f:nth-child(2) div.d3a4d table.e1fd0._4545b:nth-child(13) tbody:nth-child(2) tr:nth-child td:nth-child(6) > span.e0025").text
+  state_tests = doc.css("div.container.ddf6f:nth-child(2) div.d3a4d table.e1fd0._4545b:nth-child(13) tbody:nth-child(2) tr:nth-child(1) td:nth-child(6) > span.e0025").text
 
-  puts "#{state-date}. Total tests = #{state_tests}."
+  puts "#{state_date}. Total tests = #{state_tests}."
 
 end
 end
-end
+#end
