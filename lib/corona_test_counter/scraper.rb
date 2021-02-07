aaaba88dbb2e72c3a035d3481559f21012a5260f
Devin Wilson <craigvin84@yahoo.com>
2021-02-07 20:13:13 +0000

    switch to css

--- lib/corona_test_counter/scraper.rb
+++ lib/corona_test_counter/scraper.rb
@@ -3,12 +3,10 @@ class CoronaTestCounter::Scraper
 def self.scrape_states
 
 
+if self == "alaska"
+  doc = Nokogiri::HTML(open("https://covidtracking.com/data/state/alaska/tests-viral"))
+  state_count =  doc.xpath("//tbody/tr")
 
-  doc = Nokogiri::HTML(open("https://covidtracking.com/data/state/utah/tests-viral"))
-  state_count1 =  doc.xpath("//tbody/tr[1]/td[6]/span[2]").text
-
-  puts  "#{state_count1}"
-
-
+end
 end
 end
