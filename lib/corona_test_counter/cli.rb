849ad5091acbfd4577294471153d956024a440fe
Devin Wilson <craigvin84@yahoo.com>
2021-02-13 22:54:19 +0000

    CLI change color and get state

--- lib/corona_test_counter/cli.rb
+++ lib/corona_test_counter/cli.rb
@@ -1,65 +1,27 @@
 class CoronaTestCounter::CLI
+  @@muted = "\e[1;31m"
+  @@grn = "\e[1;32m"
+  @@blu = "\e[1;34m"
+  @@mag = "\e[1;35m"
+  @@cyn = "\e[1;36m"
+  @@white = "\e[0m"
+
   def call
-    puts "\nWelcome to the Covid Test Counter!\n".colorize(:green)
-    get_available_states
-    list_states
-    get_user_state
-    #get_counts(state)
-    #list_counts
+    puts "\n#{@@grn}Welcome to Eden!#{@@white}\n"
+    @input = ""
+    until @input == "exit"
+      get_available_states
+      list_states
+      get_user_state
+      what_next
+    end
+    goodbye
   end
 
-    def get_available_states
-      @states = ["Alaska",
-                  "Alabama",
-                  "Arkansas",
-                  "Arizona",
-                  "California",
-                  "Colorado",
-                  "Connecticut",
-                  "Delaware",
-                  "Florida",
-                  "Georgia",
-                  "Hawaii",
-                  "Iowa",
-                  "Idaho",
-                  "Illinois",
-                  "Indiana",
-                  "Kansas",
-                  "Kentucky",
-                  "Louisiana",
-                  "Massachusetts",
-                  "Maryland",
-                  "Maine",
-                  "Michigan",
-                  "Minnesota",
-                  "Missouri",
-                  "Mississippi",
-                  "Montana",
-                  "North Carolina",
-                  "North Dakota",
-                  "Nebraska",
-                  "New Hampshire",
-                  "New Jersey",
-                  "New Mexico",
-                  "Nevada",
-                  "New York",
-                  "Ohio",
-                  "Oklahoma",
-                  "Oregon",
-                  "Pennsylvania",
-                  "Rhode Island",
-                  "South Carolina",
-                  "South Dakota",
-                  "Tennessee",
-                  "Texas",
-                  "Utah",
-                  "Virginia",
-                  "Vermont",
-                  "Washington",
-                  "Wisconsin",
-                  "West Virginia",
-                  "Wyoming"]
-                end
+  def get_available_states
+      @states = Corona::State.states
+  end
+
   def list_states
     #list states
     puts "Please type in the state as it appears for the state's data.".colorize(:yellow)
