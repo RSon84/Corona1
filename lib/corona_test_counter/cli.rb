7e8aebfcf91813fcb15809cd8164be2cfda7572c
Devin Wilson <craigvin84@yahoo.com>
2021-02-20 21:10:07 +0000

    update cli

--- lib/corona_test_counter/cli.rb
+++ lib/corona_test_counter/cli.rb
@@ -7,7 +7,7 @@ class CoronaTestCounter::CLI
   @@white = "\e[0m"
 
   def call
-    puts "\n#{@@grn}Welcome to Eden!#{@@white}\n"
+    puts "\n#{@@grn}Welcome to the Covid State Test Counter#{@@white}\n"
     @input = ""
     until @input == "exit"
       get_available_states
@@ -19,17 +19,18 @@ class CoronaTestCounter::CLI
   end
 
   def get_available_states
-      @states = Corona::State.states
+      @states = Corona::Each_State.states
   end
 
   def list_states
-    puts 'Choose a month to see events.'
+    puts 'Choose a state to see the daily test counts.'
     @states.each.with_index(1) do |state, index|
       puts "#{state[2]}: #{state[0]}"
     end
   end
 
   def get_user_state
+    puts "Please input the state abbreviation you would like to review."
     chosen_state = gets.strip
     show_data_for(chosen_state) if valid_input(chosen_state)
     end
@@ -40,11 +41,25 @@ class CoronaTestCounter::CLI
     codes.include?(input)
   end
 
-  def show_data_for(chosen_state)
-    # state = @states[chosen_state.downcase]
-    puts "Here are daily increases in testing for #{chosen_state}.".red
-    CoronaTestCounter::Scraper.scrape_states
-    ## To implement
-    # CoronaTestCounter::Dates.all.each.with_index(1) do | date|
-    #   puts date.
+  def show_state_test_viral(chosen_month)
+    state = Corona::State.get_state_url(chosen_month)
+    url = "https://covidtracking.com#{state[1]}/tests-viral"
+    puts url
+    rows = Corona::Scraper.scrap_table(url)
+    puts "#{@@grn}Data For #{state[0]} state is following#{@@white}"
+    rows.each_with_index do |row, index|
+      current = row
+      previous = rows[index + 1]
+      puts ("#{current.keys.last} on #{@@blu} #{current.values.first} #{@@white} =  #{@@grn} #{current.values.last.tr(",", "").to_i - previous.values.last.tr(",", "").to_i} #{@@white}") if previous
+    end
   end
+
+  def what_next
+    puts "#{@@blu}Are you done? Type 'exit' to exit or hit any key to see another state tests#{@@white}"
+    @input = gets.strip
+  end
+
+  def goodbye
+    puts "Program Ended"
+  end
+end
